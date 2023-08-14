e3e935763c9eff4b556e9118a35ae249cfde93de
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2023-08-14 19:00:43 +0300

    mvc

--- controller.py
+++ controller.py
@@ -4,25 +4,18 @@ from model import Model
 from view import Ui_main_window, Ui_window_login, Ui_Form
 from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QPushButton
 from PySide6 import QtWidgets, QtGui
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
+
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
 
-from selenium.webdriver.edge.service import Service as EdgeService
-from subprocess import CREATE_NO_WINDOW # This flag will only be available in windows
+from selenium.webdriver.common.by import By
 
 max_course_per_term = 15
 max_term = 5
 
 class Controller:
     def __init__(self):
-        self.driver = None
-
         self.model = Model()
         self.main = QtWidgets.QMainWindow()
         self.main_window = Ui_main_window()
@@ -66,7 +59,7 @@ class Controller:
         self.login.show()
 
     def disconnect(self):
-        self.driver.close()
+        self.model.driver.close()
         self.driver = None
         self.main_window.menu.setTitle("İTÜ ÖBS'ne Giriş Yap")
         self.main_window.button_pull.setEnabled(False)
@@ -74,62 +67,20 @@ class Controller:
         self.option_disconnect.setVisible(False)
 
     def connect(self):
-        edge_options = Options()
-        edge_options.add_argument("--headless")
         if self.login_window.input_username.text() == "" or self.login_window.input_password.text() == "" or self.login_window.input_pin.text() == "":
             self.show_error("Lütfen tüm alanları doldurunuz.")
             return
-        try:
-            self.driver = webdriver.Edge(options=edge_options)
-            self.driver.get("http://uzay.sis.itu.edu.tr/login/index.php")
-
-            page_content = self.driver.page_source
-
-            if "Tüm İTÜ hizmetleri" in page_content:
-                username_input = self.driver.find_element("id", "ContentPlaceHolder1_tbUserName")
-                password_input = self.driver.find_element("id", "ContentPlaceHolder1_tbPassword")
-                login_button = self.driver.find_element("id", "ContentPlaceHolder1_btnLogin")
-                
-                username_input.send_keys(self.login_window.input_username.text())
-                password_input.send_keys(self.login_window.input_password.text())
-                login_button.click()
-
-                page_content = self.driver.page_source
-                
-                if "Kullanıcı adı veya şifre hatalı." in page_content:
-                    self.show_error("Kullanıcı adı ya da şifre hatalı.")
-                    self.driver.close()
-
-                else:
-                    page_content_before = self.driver.page_source
-
-                    pin_input = self.driver.find_element("name", "PIN")
-                    pin_input.send_keys(self.login_window.input_pin.text())
-                    login_obs = self.driver.find_element(By.XPATH, "/html/body/div[3]/form/p/input[1]")
-                    login_obs.click()
-
-                    page_content_after = self.driver.page_source
-                    
-                    if page_content_before == page_content_after:
-                        self.error.show()
-                        self.show_error("Pin hatalı.")
-                        self.driver.close()
-                    else:
-                        ogrenci_servisi = self.driver.find_element(By.XPATH, "/html/body/div[3]/table[2]/tbody/tr[1]/td[2]/a")
-                        ogrenci_servisi.click()
-                        ogrenci_bilgileri = self.driver.find_element(By.XPATH, "/html/body/div[3]/table[1]/tbody/tr[2]/td[2]/a")
-                        ogrenci_bilgileri.click()
-                        transkript = self.driver.find_element(By.XPATH, "/html/body/div[3]/table[1]/tbody/tr[3]/td[2]/a")
-                        transkript.click()
-                        self.main_window.button_pull.setEnabled(True)
-                        self.login.close()
-                        self.main_window.menu.setTitle(f"Kullanıcı: {self.login_window.input_username.text()}")
-                        self.option_disconnect.setVisible(True)
-                        self.option_connect.setVisible(False)
-        except:
-            self.error.show()
-            self.show_error("İTÜ ÖBS'ne ulaşılamadı.")
-            self.driver.close()
+        else:
+            result = self.model.connect_to_system(self.login_window.input_username.text(), self.login_window.input_password.text(), self.login_window.input_pin.text())
+            if result != "":
+                self.error_window.label.setText(result)
+                self.error.show()
+            else:
+                self.main_window.button_pull.setEnabled(True)
+                self.login.close()
+                self.main_window.menu.setTitle(f"Kullanıcı: {self.login_window.input_username.text()}")
+                self.option_disconnect.setVisible(True)
+                self.option_connect.setVisible(False)
 
     def pull(self):
         try:
@@ -139,9 +90,7 @@ class Controller:
                         self.main_window.table.insertRow(self.main_window.table.rowCount())
                         for y in range(1, 5):
                             try:
-                                row_value = self.driver.find_element(By.XPATH, f"/html/body/div[3]/table/tbody/tr/td/table[2]/tbody/tr[{z}]/td[1]/table/tbody/tr[{x}]/td[{y}]/font").text
-                                print(row_value)
-                                
+                                row_value = self.model.driver.find_element(By.XPATH, f"/html/body/div[3]/table/tbody/tr/td/table[2]/tbody/tr[{z}]/td[1]/table/tbody/tr[{x}]/td[{y}]/font").text
                                 item1 = QTableWidgetItem(row_value)
                                 self.main_window.table.setItem(self.main_window.table.rowCount() - 1, y - 1, item1)
                             except:
@@ -161,6 +110,7 @@ class Controller:
             self.main_window.button_delete_course.setEnabled(True)
             self.main_window.button_calculate.setEnabled(True)
             self.main_window.button_clear_table.setEnabled(True)
+        self.main_window.label.setText("")
 
     def delete_row(self):
         selected_ranges = self.main_window.table.selectedRanges()
@@ -177,6 +127,7 @@ class Controller:
             self.main_window.button_clear_table.setEnabled(False)
             self.main_window.button_calculate.setEnabled(False)
             self.main_window.button_pull.setEnabled(True)
+        self.main_window.label.setText("")
 
     def calculate(self):
         self.model.course_letter_grades.clear()
@@ -205,3 +156,4 @@ class Controller:
         self.main_window.button_clear_table.setEnabled(False)
         self.main_window.button_pull.setEnabled(True)
         self.main_window.button_calculate.setEnabled(False)
+        self.main_window.label.setText("")
--- model.py
+++ model.py
@@ -1,5 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+
 class Model:
     def __init__(self):
+        self.driver = None
         self.course_letter_grades = []
         self.course_credits = []
         self.letter_grade_dict = {"AA": 4.0, "BA": 3.5, "BB": 3.0, "CB": 2.5, "CC": 2.0, "DC": 1.5, "DD": 1.0, "FF": 0.0, "VF": 0.0, "BL": 0.0, "BZ": 0.0}
@@ -11,3 +17,53 @@ class Model:
             total += self.letter_grade_dict[x] * float(self.course_credits[index])
             index += 1
         return total / sum(self.course_credits)
+    
+    def connect_to_system(self, username, password, pin):
+        try:
+            edge_options = Options()
+            edge_options.add_argument("--headless")
+            self.driver = webdriver.Edge(options=edge_options)
+            self.driver.get("http://uzay.sis.itu.edu.tr/login/index.php")
+
+            page_content = self.driver.page_source
+
+            if "Tüm İTÜ hizmetleri" in page_content:
+                username_input = self.driver.find_element("id", "ContentPlaceHolder1_tbUserName")
+                password_input = self.driver.find_element("id", "ContentPlaceHolder1_tbPassword")
+                login_button = self.driver.find_element("id", "ContentPlaceHolder1_btnLogin")
+                
+                username_input.send_keys(username)
+                password_input.send_keys(password)
+                login_button.click()
+
+                page_content = self.driver.page_source
+                
+                if "Kullanıcı adı veya şifre hatalı." in page_content:
+                    self.driver.close()
+                    return "Kullanıcı adı ya da şifre hatalı."
+                else:
+                    page_content_before = self.driver.page_source
+
+                    pin_input = self.driver.find_element("name", "PIN")
+                    pin_input.send_keys(pin)
+                    login_obs = self.driver.find_element(By.XPATH, "/html/body/div[3]/form/p/input[1]")
+                    login_obs.click()
+
+                    page_content_after = self.driver.page_source
+                    
+                    if page_content_before == page_content_after:
+                        self.driver.close()
+                        return "Pin hatalı."
+                    else:
+                        ogrenci_servisi = self.driver.find_element(By.XPATH, "/html/body/div[3]/table[2]/tbody/tr[1]/td[2]/a")
+                        ogrenci_servisi.click()
+                        ogrenci_bilgileri = self.driver.find_element(By.XPATH, "/html/body/div[3]/table[1]/tbody/tr[2]/td[2]/a")
+                        ogrenci_bilgileri.click()
+                        transkript = self.driver.find_element(By.XPATH, "/html/body/div[3]/table[1]/tbody/tr[3]/td[2]/a")
+                        transkript.click()
+                        return ""
+        except:
+            self.driver.close()
+            return "İTÜ ÖBS'ne ulaşılamadı."
+        
+    
